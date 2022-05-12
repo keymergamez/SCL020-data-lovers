cb099dbdfa8ffa7679eb8f4cc1a3677ba7472c84
keymergamez <keymergamez@gmail.com>
2022-05-12 11:22:51 -0400

    mostar Data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,31 @@
-// estas funciones son de ejemplo
+    // // estas son funciones 
+export const getRhythmicGymnastics = (data) => { 
+    return data.filter(
+     (currentAthlete) => 
+       currentAthlete.event === "Rhythmic Gymnastics Women's Group"
 
-export const example = () => {
-  return 'example';
+    );
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const showAthletes = (athletes) => {
+  return athletes
+    .map(
+      (currentAthlete) => 
+      `<div class="athlete">
+        <img athlete/>
+        <h3>${currentAthlete.name}</h3>
+        <p>${currentAthlete.team}</p>
+        <button> Ver Atletas </button>
+      </div>`
+    )
+    .join("");
+}; 
+  
+/* 
+ console.log(getElementsByType(athletes, "team"))
+ console.log(getAthletesByTeam(athletes, "Italy"))
+console.log(sortData(athletes, "name", "Desc")) */
+
+
+
+
